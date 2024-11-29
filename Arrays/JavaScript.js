de689a77410fc15ab37a55652ba6c1f0f0bbf870
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2024-11-29 18:38:10 -0300

    one more day studying

--- Arrays/JavaScript.js
+++ Arrays/JavaScript.js
@@ -7,3 +7,40 @@ console.log(typeof anyText.toUpperCase);
 const arr = [1, 2, 3, 4];
 
 console.log(arr[2])
+
+// Object literals
+// O object literals é uma instância de um objeto, chamado object.
+
+const person = {
+    name: "Marconny",
+    job: "Programador",
+    year: 20        
+};
+
+console.log(person);
+
+console.log(person.job)
+
+console.log(person.name.length);
+
+// Criando e deletando propiedades
+
+const car = {
+    engine: 2.0,
+    brand: "volkswagen",
+    model: "Golf",
+    km: 0
+};
+
+console.log(car);
+
+car.doors = 4;
+
+console.log(car);
+
+delete car.km;
+
+console.log(car);
+
+console.log(Object.keys(car));
+console.log(Object.entries(car));
